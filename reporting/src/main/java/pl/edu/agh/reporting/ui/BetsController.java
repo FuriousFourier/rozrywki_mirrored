7beb7e01e6d974dfbc97749873427e909a2d3cd7
hector <krzysiuk.66@gmail.com>
2018-12-01 15:05:47 +0100

    exposed bets to UI

--- reporting/src/main/java/pl/edu/agh/reporting/ui/BetsController.java
+++ reporting/src/main/java/pl/edu/agh/reporting/ui/BetsController.java
@@ -1,6 +1,5 @@
 package pl.edu.agh.reporting.ui;
 
-import com.google.common.collect.ImmutableList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.edu.agh.reporting.domain.bet.Bet;
@@ -19,9 +18,7 @@ public class BetsController {
 
     @GetMapping("/api/bets")
     public List<Bet> getAllBets() {
-        // TODO
-//        return betRepository.findAll();
-        return ImmutableList.of(new Bet("1", "match", "Tom"));
+        return betRepository.findAll();
     }
 
 }
