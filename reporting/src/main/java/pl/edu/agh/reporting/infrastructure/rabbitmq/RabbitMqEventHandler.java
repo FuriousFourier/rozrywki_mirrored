54ae076e38f5d4313e02242a693d6f5bb6ac6e92
hector <krzysiuk.66@gmail.com>
2018-12-01 14:16:25 +0100

    RabbitMQ event handler

--- reporting/src/main/java/pl/edu/agh/reporting/infrastructure/rabbitmq/RabbitMqEventHandler.java
+++ reporting/src/main/java/pl/edu/agh/reporting/infrastructure/rabbitmq/RabbitMqEventHandler.java
@@ -1,14 +1,19 @@
 package pl.edu.agh.reporting.infrastructure.rabbitmq;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import pl.edu.agh.reporting.events.ReportingEvent;
 
 @EnableBinding(Sink.class)
+@Slf4j
 public class RabbitMqEventHandler {
 
+    @StreamListener(Sink.INPUT)
     public void handleEvent(ReportingEvent reportingEvent) {
-
+        // TODO
+        log.info("Received event {}", reportingEvent);
     }
 
 }
