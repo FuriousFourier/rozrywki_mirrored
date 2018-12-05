b89248f1bf6ca3644b14d96f5c056d75fccc947b
hector <krzysiuk.66@gmail.com>
2018-12-06 00:30:21 +0100

    cleanup

--- reporting/src/main/java/pl/edu/agh/reporting/ui/ReportsController.java
+++ reporting/src/main/java/pl/edu/agh/reporting/ui/ReportsController.java
@@ -1,19 +1,11 @@
 package pl.edu.agh.reporting.ui;
 
-import pl.edu.agh.reporting.domain.site.BetTypeReport;
-import pl.edu.agh.reporting.domain.site.OperatorReport;
 import pl.edu.agh.reporting.domain.site.SiteReport;
 
 import java.util.List;
 
 public class ReportsController {
 
-    public List<BetTypeReport> getBetTypeReports() {
-        return null;
-    }
-    public List<OperatorReport> getOperatorReports() {
-        return null;
-    }
     public List<SiteReport> getSiteReports() {
         return null;
     }
