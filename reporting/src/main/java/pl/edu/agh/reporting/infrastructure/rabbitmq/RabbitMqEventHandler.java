231a586ed836e8edb7498fceb0892d458a549e89
hector <krzysiuk.66@gmail.com>
2018-12-12 04:00:53 +0100

    rabbitmq client in core fixed + handled BSF event

--- reporting/src/main/java/pl/edu/agh/reporting/infrastructure/rabbitmq/RabbitMqEventHandler.java
+++ reporting/src/main/java/pl/edu/agh/reporting/infrastructure/rabbitmq/RabbitMqEventHandler.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.reporting.infrastructure.rabbitmq;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.function.json.JacksonMapper;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
@@ -8,7 +9,6 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 import pl.edu.agh.reporting.ReportingEventType;
 import pl.edu.agh.reporting.application.ReportingApplicationService;
-import pl.edu.agh.reporting.events.BetSlipPlaced;
 import pl.edu.agh.reporting.events.ReportingEvent;
 
 @EnableBinding(Sink.class)
@@ -17,25 +17,32 @@ public class RabbitMqEventHandler {
 
     private final ReportingApplicationService reportingApplicationService;
 
-    public RabbitMqEventHandler(ReportingApplicationService reportingApplicationService) {
+    private final JacksonMapper jacksonMapper;
+
+    public RabbitMqEventHandler(ReportingApplicationService reportingApplicationService, JacksonMapper jacksonMapper) {
         this.reportingApplicationService = reportingApplicationService;
+        this.jacksonMapper = jacksonMapper;
     }
 
     @StreamListener(Sink.INPUT)
-    public void handleEvent(Message<BetSlipPlaced> message) {
-        dispatchEvent(message);
-    }
-
-    private void dispatchEvent(Message<? extends ReportingEvent> message) {
+    public void handleEvent(Message<String> message) {
         log.info("Received event {}", message);
 
         final MessageHeaders headers = message.getHeaders();
-        final ReportingEvent reportingEvent = message.getPayload();
 
-        final String textEventType = headers.get("type", String.class);
-        final ReportingEventType eventType = ReportingEventType.valueOf(textEventType);
+        final ReportingEventType eventType = resolveEventType(headers);
+        final ReportingEvent reportingEvent = deserializeEvent(message, eventType);
 
         reportingApplicationService.handle(reportingEvent, eventType);
+    }
+
+    private ReportingEventType resolveEventType(MessageHeaders headers) {
+        final String textEventType = headers.get("type", String.class);
+        return ReportingEventType.valueOf(textEventType);
+    }
+
+    private ReportingEvent deserializeEvent(Message<String> message, ReportingEventType eventType) {
+        return jacksonMapper.toObject(message.getPayload(), eventType.getEventClass());
     }
 
 }
--- reportingapi/src/main/java/pl/edu/agh/reporting/ReportingEventType.java
+++ reportingapi/src/main/java/pl/edu/agh/reporting/ReportingEventType.java
@@ -1,9 +1,19 @@
 package pl.edu.agh.reporting;
 
+import lombok.Getter;
+import pl.edu.agh.reporting.events.*;
+
 public enum ReportingEventType {
-    BET_SLIP_FINISHED,
-    BET_SLIP_GONE_LIVE,
-    BET_SLIP_PLACED,
-    PAY_IN_CREDITED,
-    PAY_OUT_CLAIMED
+    BET_SLIP_FINISHED(BetSlipFinished.class),
+    BET_SLIP_GONE_LIVE(BetSlipGoneLive.class),
+    BET_SLIP_PLACED(BetSlipPlaced.class),
+    PAY_IN_CREDITED(PayInCredited.class),
+    PAY_OUT_CLAIMED(PayOutClaimed.class);
+
+    @Getter
+    private final Class<? extends ReportingEvent> eventClass;
+
+    ReportingEventType(Class<? extends ReportingEvent> eventClass) {
+        this.eventClass = eventClass;
+    }
 }
